bf54e62e26aee8fe01da2278763ecb581fea67e5
Andre Eife <andre.eife@gmail.com>
2012-07-15 11:18:33 +0200

    bugfixes

--- app/js/helpers.js
+++ app/js/helpers.js
@@ -1,13 +1,16 @@
 if (!Array.prototype.indexOf) {
-    Array.prototype.indexOf = function(elt /*, from*/) {
-        var len = this.length;
+    Array.prototype.indexOf = function(e /*, from*/) {
+        var length = this.length;
         var from = Number(arguments[1]) || 0;
-        from = (from < 0) ? Math.ceil(from) : Math.floor(from);
+
         if (from < 0) {
-            from += len;
+            from = Math.ceil(from) + length;
+        } else {
+            from = Math.floor(from);
         }
-        for (; from < len; ++from) {
-            if (from in this && this[from] === elt) {
+
+        for (; from < length; ++from) {
+            if (from in this && this[from] === e) {
                 return from;
             }
         }
@@ -18,7 +21,13 @@ if (!Array.prototype.indexOf) {
 if (!Array.prototype.remove) {
     Array.prototype.remove = function(from, to) {
         var rest = this.slice((to || from) + 1 || this.length);
-        this.length = from < 0 ? this.length + from : from;
+
+        if (from < 0) {
+             this.length = this.length + from;
+        } else {
+             this.length = from;
+        }
+
         return this.push.apply(this, rest);
     };
 }
